09360a2494b07c5689b8c7557fc9aeacef0bb0e2
metafysika <deeevangelista@outlook.com>
2025-08-27 09:19:57 +0800

    Migrated to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Romanizer from './components/Romanizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Romanizer />
+    </div>
+  );
+}
+
+export default App;
